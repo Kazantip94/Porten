194e4594b133d46d00bba7d3be74cb398ac61f4c
Serhii <blackdato@gmail.com>
2021-06-04 22:49:21 +0300

    fix all bugs

--- js/main.js
+++ js/main.js
@@ -1,71 +1,80 @@
 $(document).ready(function(){
     $('.brands__mobile').slick({
-        arrows: true,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '60px',
         slidesToShow: 3,
-        slidesToScroll: 1,
-        draggable: false,
-        // centerMode: true
         responsive: [
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2
             }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
         ]
     });
     $('.receipts__mobile').slick({
-        arrows: true,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '60px',
         slidesToShow: 3,
-        slidesToScroll: 1,
-        draggable: false,
-        // centerMode: true
         responsive: [
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2
             }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
         ]
     });
     $('.season__mobile').slick({
         arrows: false,
+        centerMode: true,
+        centerPadding: '60px',
         slidesToShow: 3,
-        slidesToScroll: 1,
-        draggable: true,
-        centerMode: false,
         responsive: [
-            {
-                breakpoint: 830,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2
             }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
         ]
     });
 });
